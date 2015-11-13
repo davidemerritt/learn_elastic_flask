0e7da41348dd65e9cf1ca53db60f3e65dbfa3844
David Merritt <david.e.merritt@gmail.com>
2015-11-13 13:17:18 -0800

    fix against pylint

--- flask_app/base.py
+++ flask_app/base.py
@@ -1,8 +1,10 @@
+""" Let's test out elastic search and flask combination """
 from flask import Flask
-app = Flask(__name__)
+app = Flask(__name__) # pylint:disable=C0103
 
 @app.route("/")
 def run_me():
+    """ Base run section """
     return "Hello World!"
 
 if __name__ == "__main__":
